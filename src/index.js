5913fae83d3cc66b481cbee11a22be0617ec8d47
Rohit chaudhary <151979116+Rohit-Chaudhary86@users.noreply.github.com>
2025-05-28 10:45:35 +0530

    removed isAuth

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/api",apiRouter)  //changed
 
 
-app.get("/ping",isAuthenticated,(req,res)=>{
+app.get("/ping",(req,res)=>{
     console.log(req.body)
     console.log(req.query)
     console.log(req.user)
